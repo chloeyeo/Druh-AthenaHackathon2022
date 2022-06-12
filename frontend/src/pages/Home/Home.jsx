7ac48514fc2b0ca6c79f91f1b5d98548906c7048
Nicole Shek <Nicole@172-10-21-12.lightspeed.clmasc.sbcglobal.net>
2022-06-12 10:34:45 +0100

    sttyle homepage

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,28 +1,46 @@
 import React from "react";
 import "./Home.scss";
 import { Link } from "react-router-dom";
+import logo from "../../assets/images/logo.png";
+import ukflag from "../../assets/images/flaguk.png";
+import ukraine from "../../assets/images/flagukraine.png";
+import russia from "../../assets/images/flagrussia.png";
 
 const Home = () => {
   return (
-    <form className="languageForm">
-      <ul>
-        <li>
-          <input id="english" type="radio" name="language" />
-          <label htmlFor="english">English</label>
-        </li>
-        <li>
-          <input id="ukraine" type="radio" name="language" />
-          <label htmlFor="ukraine">Ukrainian</label>
-        </li>
-        <li>
-          <input id="russian" type="radio" name="language" />
-          <label htmlFor="russian">Russian</label>
-        </li>
-      </ul>
-      <Link className="primaryButton" to="/signup">
-        Next
-      </Link>
-    </form>
+    <div className="home">
+      <div className="leftside">
+        <img src={logo} alt="Druh logo" />
+      </div>
+      <div className="rightside">
+        <p>
+          Connect your child with other Ukrainian children from across the
+          United Kingdom.
+        </p>
+        <form className="languageForm">
+          <ul>
+            <li>
+              <input id="english" type="radio" name="language" />
+              <img src={ukflag} alt="uk flag" />
+              <label htmlFor="english">English</label>
+            </li>
+            <li>
+              <input id="ukraine" type="radio" name="language" />
+              <img src={ukraine} alt="ukraine flag" />
+              <label htmlFor="ukraine">Ukrainian</label>
+            </li>
+            <li>
+              <input id="russian" type="radio" name="language" />
+              <img src={russia} alt="russia flag" />
+              <label htmlFor="russian">Russian</label>
+            </li>
+          </ul>
+          <Link className="primaryButton" to="/signup">
+            Next
+          </Link>
+        </form>
+      </div>
+    </div>
   );
 };
 
